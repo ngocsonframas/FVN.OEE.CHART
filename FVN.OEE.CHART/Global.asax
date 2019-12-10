8adb5ccb4bb746a57b58f99ca672f16c2f41681c
ngocsonframas <nguyenngocson9556@gmail.com>
2019-12-10 08:38:44 +0700

    First comnmit

---
+++ FVN.OEE.CHART/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FVN.OEE.CHART.MvcApplication" Language="C#" %>
---
+++ FVN.OEE.CHART/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FVN.OEE.CHART.Startup))]
+namespace FVN.OEE.CHART
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
